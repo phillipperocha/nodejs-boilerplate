283c84e6d805a89a3f7ad366a924f354f973e174
Phillippe Rocha <phillipperocha@gmail.com>
2020-01-03 23:59:52 -0300

    Creating Model Auto Loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Phillippe Rocha',
+    email: 'phillipperocha@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
