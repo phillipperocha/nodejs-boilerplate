53ba9f149441271a3c26a620fb1cb4400d40edaf
Phillippe Rocha <phillipperocha@gmail.com>
2020-01-04 00:04:44 -0300

    Creating users

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Phillippe Rocha',
-    email: 'phillipperocha@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.get('/users', UserController.store);
 
 export default routes;
