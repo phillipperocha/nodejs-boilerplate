c186173b09bf51bb9f328cc1fc9062291bffff07
Phillippe Rocha <phillipperocha@gmail.com>
2020-01-09 21:47:11 -0300

    Adding an avatar to the user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -27,6 +27,11 @@ class User extends Model {
     return this;
   }
 
+  // We do the associate method to make relationships between entities
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
